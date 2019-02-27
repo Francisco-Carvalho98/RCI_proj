0816eae70e23f5d04dbb56d285340bfc0bdcf418
francisco <quelincho@gmail.com>
2019-02-27 19:09:23 +0000

    Cuckiness

--- proj.c
+++ proj.c
@@ -9,12 +9,15 @@
 int main ( int argc, char *argv[] )
 {
     struct input input;
+    char buffer[64];
 
     system("clear");
 
     inputHandler(&input, argv, argc);
 
-
+    printf("iamroot> ");
+    fgets(buffer, 10, stdin);
+    printf("Echo: %s\n", buffer);
 
     return 0;
 }
